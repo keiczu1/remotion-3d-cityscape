f34be681fefbcbf36bb0da3bdd5a0661ebb2c856
keiczu33 <keiczu33@gmail.com>
2026-03-11 11:23:21 +0300

    Revert "feat: add curated appearance sound library config"
    This reverts commit 15929017ac43beb43a13c7575e92cdb405bffa59.

--- src/compositions/ranking-towers/tests/scene-logic.test.ts
+++ src/compositions/ranking-towers/tests/scene-logic.test.ts
@@ -10,7 +10,6 @@ import {
     type AppearanceEvent,
     type ResolvedSoundCue,
 } from "../model/types";
-import { appearanceSoundLibrary } from "../sound/appearance-library";
 import {
     baseDurationInFrames,
     durationInFrames,
@@ -200,25 +199,4 @@ test("tech descriptors resolve to tech-reveal", () => {
         }),
         "tech-reveal",
     );
-});
-
-test("appearance sound library covers every supported profile", () => {
-    for (const profile of appearanceSoundProfiles) {
-        const recipes = appearanceSoundLibrary[profile];
-
-        assert.ok(Array.isArray(recipes));
-        assert.ok(recipes.length > 0);
-    }
-});
-
-test("appearance sound library uses public-relative asset paths", () => {
-    for (const recipes of Object.values(appearanceSoundLibrary)) {
-        for (const recipe of recipes) {
-            const layers = [recipe.transient, recipe.body, recipe.tail].filter(Boolean);
-
-            for (const layer of layers) {
-                assert.match(layer.src, /^sfx\/appearance\/.+\.(wav|mp3)$/);
-            }
-        }
-    }
 });
